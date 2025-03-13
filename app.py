fcbd44df5ec05a49305370f14964d93d065db56d
fatwang2 <wangding0798@gmail.com>
2025-03-13 17:32:27 +0800

    modified:   app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ async def search():
     results = []
     with DDGS() as ddgs:
         # 使用DuckDuckGo搜索关键词
-        ddgs_gen = ddgs.text(keywords,safesearch='Off', timelimit='y', backend="lite")
+        ddgs_gen = ddgs.text(keywords,safesearch='Off', timelimit='y')
         # 从搜索结果中获取最大结果数
         for r in islice(ddgs_gen, max_results):
             results.append(r)
